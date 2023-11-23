1c946ead97d4c3c8e1d6ea6f77a578b915c1fed1
Samuel <samuelamat.alu@iespacomolla.es>
2023-11-23 17:34:31 +0100

    diseño inicio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				inter: ['Inter Variable', 'sans-serif', ...defaultTheme.fontFamily.sans],
+			  },
+			colors: {
+				verde: "#58d68d"
+			},
+		},
 	},
 	plugins: [],
 }
